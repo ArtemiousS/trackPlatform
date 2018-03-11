6ef71f507edd2cd1e691d9d1c247ab7c47a85ab0
ArtemiousS <artem.klimec8@gmail.com>
2018-03-11 12:50:53 +0300

    Add files via upload
    C# code

---
+++ code/Data.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+struct Data
+{
+    function<void()> callback;
+	volatile bool pIsRepeat;
+    UInt32 frequency;
+};
+
+class AutoConnector
+{
+    
+	private Thread pConnectingThread;
+    private function<void()> callback;
+	private volatile bool isRepeat;
+    private UInt32 frequency;
+
+   	/**
+	 * @param c Callback to run by timer
+	 * @param f Time to wait before next callback call in milliseconds
+	 */
+	public AutoConnector(std::function<void()> c, UInt32 f);
+    public ~AutoConnector();
+    public void start();
+    public void stop();
+    public bool isConnecting();
+};
---
+++ code/Logger.cs
@@ -0,0 +1,39 @@
+﻿//#define _LOGGER_H_
+//#if _LOGGER_H_
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApplication8
+{
+
+/**
+ * @brief If defined, log is on, else off
+ */
+#if LOGGER_ON
+    class Logger
+{
+    public static void log(string s);
+};
+
+#else
+class Logger
+{
+    static Logger logger;
+
+    const string way = "logger.log";
+        FileStream file;
+
+    Logger();
+    void logOneString(string s);
+
+    
+	public static void log(string s);
+};
+#endif
+}
+//#endif //* _LOGGER_H_ */
